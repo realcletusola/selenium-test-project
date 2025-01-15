545e2f2335f3419ba08cdc522ae1f9a3096a8d84
cletus <cletushunsu@gmail.com>
2025-01-15 08:50:25 +0100

    Initial commit for app set up, AutoComplete and Keyboardtest

---
+++ src/test/java/com/example/AutocompleteTest.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class AutocompleteTest {
+    @Test
+    public void testAutocomplete() throws InterruptedException  {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("1555 Park Blvd, Palo Alto, CA");
+        Thread.sleep(1000);
+
+        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        autocompleteResult.click();
+
+        driver.quit();
+    }
+    
+}
---
+++ src/test/java/com/example/KeyboardAndMouseInputTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class KeyboardAndMouseInputTest {
+    @Test
+    public void testKeyboardAndMouseInput() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        
+        ChromeOptions options = new ChromeOptions();
+        // options.setBinary("/usr/bin/chromium-browser"); 
+        options.addArguments("--remote-allow-origins=*");
+        
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://formy-project.herokuapp.com/keypress");
+
+        WebElement name = driver.findElement(By.id("name"));
+        name.sendKeys("Real Ola");
+
+        WebElement button = driver.findElement(By.id("button"));
+        button.click();
+
+        driver.quit();
+    }
+}
